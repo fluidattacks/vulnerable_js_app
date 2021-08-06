83b9437499a3d0e8c9e48c4a67f62a35d5c90e25
Diego Restrepo <restrepomesadiego@gmail.com>
2021-08-06 11:36:33 -0500

    feat(back): #2.2 add test002

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 var test001 = require("./testcases/test001");
+var test002 = require("./testcases/test002");
 
-router.use(test001);
+router.use(test001, test002);
 
 module.exports = router;
