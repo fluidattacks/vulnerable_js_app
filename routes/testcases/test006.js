fc151c6fdf54634eae99e74186adba128d52509c
Diego Restrepo <restrepomesadiego@gmail.com>
2021-08-18 15:31:06 -0500

    feat(back): #2.4 add more test cases
    - add 006, use an alias for exec function
    - add 007, use request body

---
+++ routes/testcases/test006.js
@@ -0,0 +1,22 @@
+const command = require("child_process").exec;
+var express = require('express');
+var router = express.Router();
+
+router.get('/test006/:user', function (req, res) {
+  var user = req.params['user'];
+  command("ls test/test_files/users/" + user + "/", (error, stdout, stderr) => {
+    var user_files = [];
+    for (file of stdout.split('\n')) {
+      if (file) {
+        user_files.push(file);
+      }
+    }
+
+    var data = {
+      'user_files': user_files
+    };
+    res.json(data);
+  });
+});
+
+module.exports = router;
---
+++ routes/testcases/test007.js
@@ -0,0 +1,24 @@
+const process = require("child_process");
+var express = require('express');
+var router = express.Router();
+
+router.get('/test007', function (req, res) {
+  let body = req.body;
+
+  process.exec("ls test/test_files/users/" + body['user'] + "/", (error, stdout, stderr) => {
+
+    var user_files = [];
+    for (file of stdout.split('\n')) {
+      if (file) {
+        user_files.push(file);
+      }
+    }
+
+    var data = {
+      'user_files': user_files
+    };
+    res.json(data);
+  });
+});
+
+module.exports = router;
