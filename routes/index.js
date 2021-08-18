7da6f5b8ebce92565abda6b2755546e6ae56cc7a
Diego Restrepo <restrepomesadiego@gmail.com>
2021-08-18 15:31:06 -0500

    feat(back): #2.4 add more test cases
    - add 006, use an alias for exec function
    - add 007, use request body

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,9 @@ var test002 = require("./testcases/test002");
 var test003 = require("./testcases/test003");
 var test004 = require("./testcases/test004");
 var test005 = require("./testcases/test005");
+var test006 = require("./testcases/test006");
+var test007 = require("./testcases/test007");
 
-router.use(test001, test002, test003, test004, test005);
+router.use(test001, test002, test003, test004, test005, test006, test007);
 
 module.exports = router;
