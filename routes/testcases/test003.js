27e8dc8e31b1bd76b12569053f41de9a7009e004
Diego Restrepo <restrepomesadiego@gmail.com>
2021-08-18 12:08:18 -0500

    feat(back): # add more test cases
    - add 003, use template strings
    - add 004, use query params
    - add 005,  use function from module
    - add request tests

---
+++ routes/testcases/test003.js
@@ -0,0 +1,25 @@
+const {
+  exec
+} = require("child_process");
+var express = require('express');
+var router = express.Router();
+
+router.get('/test003/:user', function (req, res) {
+  var user = req.params['user'];
+  exec(`ls test/test_files/users/${user}/`, (error, stdout, stderr) => {
+
+    var user_files = [];
+    for (file of stdout.split('\n')) {
+      if (file) {
+        user_files.push(file);
+      }
+    }
+
+    var data = {
+      'user_files': user_files
+    };
+    res.json(data);
+  });
+});
+
+module.exports = router;
