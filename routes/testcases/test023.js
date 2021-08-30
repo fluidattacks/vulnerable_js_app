1d881e02c6078e6f7921a039a4b0be48236d260a
Diego Restrepo <restrepomesadiego@gmail.com>
2021-08-30 09:56:44 -0500

    feat(back): #2.8 add more test cases
    - test 019, use a concatenated path
    - test 020, use template string to create path
    - test 021, user query parameters
    - test 022, add file extension to end of string
    - test 023, use concatenated string with file extension
    - test 024, check if the path contains path reverse
    - test 025, use path.join
    - test 026, use path.join
    - test 027, use path.join with template string, with file extension

---
+++ routes/testcases/test023.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+const fs = require('fs');
+
+router.get('/test023/', function (req, res) {
+  var user = req.query['user'];
+  var fileName = req.query['fileName'];
+  var filePath = "target/user_files/" + user + "/" + fileName + ".yaml";
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+      res.status('404').json({
+        'error': 'file does not exists '
+      })
+    }
+    res.send(
+      data
+    )
+  })
+});
+
+module.exports = router;
---
+++ routes/testcases/test026.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+const fs = require('fs');
+const path = require('path');
+
+router.get('/test026/:user/:fileName', function (req, res) {
+  var user = req.params['user'];
+  var fileName = req.params['fileName'];
+  var filePath = path.join("target/user_files", user, fileName);
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+      res.status('404').json({
+        'error': 'file does not exists '
+      })
+    }
+    res.send(
+      data
+    )
+  })
+});
+
+module.exports = router;
