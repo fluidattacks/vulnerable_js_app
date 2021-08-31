ff3251bec086cc3b1c581e2b92b369775a3b3eb0
Diego Restrepo <restrepomesadiego@gmail.com>
2021-08-31 14:49:41 -0500

    fix(build): fluidattacks#14 add more test
    - add alias imports

--- server/routes/testcases/test007.js
+++ server/routes/testcases/test007.js
@@ -1,11 +1,11 @@
-import { exec as command } from "child_process";
-import { Router } from 'express';
-var router = Router();
+const process = require("child_process");
+var express = require('express');
+var router = express.Router();
 
 router.get('/test007', function (req, res) {
   let body = req.body;
 
-  command("ls target/user_files/" + body['user'] + "/", (error, stdout, stderr) => {
+  process.exec("ls target/user_files/" + body['user'] + "/", (error, stdout, stderr) => {
 
     var user_files = [];
     for (var file of stdout.split('\n')) {
@@ -21,4 +21,4 @@ router.get('/test007', function (req, res) {
   });
 });
 
-export default router;
+module.exports = router;
