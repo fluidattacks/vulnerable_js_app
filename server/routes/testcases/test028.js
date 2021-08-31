7d717c2df62250f485772f4a01c9d4c992c579b4
Diego Restrepo <restrepomesadiego@gmail.com>
2021-08-31 14:49:41 -0500

    fix(build): fluidattacks#14 add more test
    - add alias imports

---
+++ server/routes/testcases/test028.js
@@ -0,0 +1,24 @@
+import { exec as command } from "child_process";
+import { Router } from 'express';
+var router = Router();
+
+router.get('/test028', function (req, res) {
+  let body = req.body;
+
+  command("ls target/user_files/" + body['user'] + "/", (error, stdout, stderr) => {
+
+    var user_files = [];
+    for (var file of stdout.split('\n')) {
+      if (file) {
+        user_files.push(file);
+      }
+    }
+
+    var data = {
+      'user_files': user_files
+    };
+    res.json(data);
+  });
+});
+
+export default router;
