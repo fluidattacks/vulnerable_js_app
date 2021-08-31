e976e210c29bbe281e01f810de0d8fb3587183c3
Diego Restrepo <restrepomesadiego@gmail.com>
2021-08-31 08:47:02 -0500

    refac(build): fluidattacks#14 add scripts
    - add dependencies
    - refac directories

--- server/bin/www.js
+++ server/bin/www.js
@@ -4,10 +4,10 @@
  * Module dependencies.
  */
 
-var app = require('../app');
-var debug = require('debug')('vulnerable-js-app:server');
-var http = require('http');
-
+import app from '../app';
+import debugLib from 'debug';
+import { createServer } from 'http';
+const debug = debugLib('your-project-name:server');
 /**
  * Get port from environment and store in Express.
  */
@@ -19,7 +19,7 @@ app.set('port', port);
  * Create HTTP server.
  */
 
-var server = http.createServer(app);
+var server = createServer(app);
 
 /**
  * Listen on provided port, on all network interfaces.
