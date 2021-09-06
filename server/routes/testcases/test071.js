92ac41690702b847d566775f4edab9cefbad74e8
Diego Restrepo <restrepomesadiego@gmail.com>
2021-09-06 11:58:17 -0500

    feat(back): fluidattacks#14 add more test
    - add more complex cases

---
+++ server/routes/testcases/test071.js
@@ -0,0 +1,27 @@
+import { exec } from "child_process";
+import { Router } from "express";
+var router = Router();
+
+router.get("/test071/:user", function (req, res) {
+  var user = req.params["user"];
+  var map64 = {
+    keyA: "a_Value",
+    keyB: user,
+    keyC: "c_value",
+  };
+  exec("ls target/user_files/" + map64.keyB + "/", (error, stdout, stderr) => {
+    var user_files = [];
+    for (var file of stdout.split("\n")) {
+      if (file) {
+        user_files.push(file);
+      }
+    }
+
+    var data = {
+      user_files: user_files,
+    };
+    res.json(data);
+  });
+});
+
+export default router;
---
+++ server/routes/testcases/test073.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var router = express.Router();
+const fs = require("fs");
+
+router.get("/test073/:user", function (req, res) {
+  var user = req.params["user"];
+  var bar = "alsosafe";
+  var valuesList = [];
+  valuesList.push("safe");
+  valuesList.push(user);
+  valuesList.push("moresafe");
+  bar = valuesList[1];
+
+  let userFiles = "./target/user_files/";
+  var filePath = userFiles + bar;
+  fs.readdir(filePath, (error, files) => {
+    if (error) {
+      res.status("404").json({
+        error: `the file ${filePath} does no exist`,
+      });
+    } else {
+      res.json({ user_files: files });
+    }
+  });
+});
+
+module.exports = router;
