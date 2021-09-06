92d2e67cd435cdfb80b979d622b37e95596a463b
Diego Restrepo <restrepomesadiego@gmail.com>
2021-09-06 11:58:17 -0500

    feat(back): fluidattacks#14 add more test
    - add more complex cases

---
+++ server/routes/testcases/test072.js
@@ -0,0 +1,39 @@
+import { Router } from "express";
+import * as fileSystem from "fs";
+import { join } from "path";
+var router = Router();
+router.get("/test072", function (req, res) {
+  let body = req.body;
+
+  let user = body.user;
+  let file = body.fileName;
+  let payload = body.payload;
+
+  let usersFiles = "./target/user_files";
+
+  let result = {
+    ok: false,
+  };
+  let users = Array.from(fileSystem.readdirSync(usersFiles).values());
+  if (users.includes(user)) {
+    let filePath = join(`target/user_files/${user}/`, file + ".yaml");
+    fileSystem.writeFile(filePath, payload, "utf8", (error) => {
+      result.ok = true;
+      if (error) {
+        result.ok = false;
+        result["errors"] = [
+          {
+            message: error,
+          },
+        ];
+      } else {
+        res.json(result);
+      }
+    });
+  } else {
+    result.ok = false;
+    res.send(result);
+  }
+});
+
+export default router;
