2da6e13b1854af4e3fe17f3a1f8974a09225cbb7
Diego Restrepo <restrepomesadiego@gmail.com>
2021-09-10 08:22:04 -0500

    refac(back): fluidattacks#14 apply formatter

--- server/app.js
+++ server/app.js
@@ -1,22 +1,24 @@
-import createError from 'http-errors';
-import express, { json, urlencoded } from 'express';
-import { join } from 'path';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
+import createError from "http-errors";
+import express, { json, urlencoded } from "express";
+import { join } from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
 
-import indexRouter from './routes/index';
+import indexRouter from "./routes/index";
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(json());
-app.use(urlencoded({
-  extended: false
-}));
+app.use(
+  urlencoded({
+    extended: false,
+  })
+);
 app.use(cookieParser());
-app.use(express.static(join(__dirname, '../public')));
+app.use(express.static(join(__dirname, "../public")));
 
-app.use('/', indexRouter);
+app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -27,7 +29,7 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
