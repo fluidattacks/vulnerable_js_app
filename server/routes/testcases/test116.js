01ecbd743003193ea36d4446db540937fbda2ddf
Diego Restrepo <restrepomesadiego@gmail.com>
2021-09-17 10:34:24 -0500

    feat(back): fluidattacks#14 add more test
    - add crypto tests

---
+++ server/routes/testcases/test116.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+var CryptoJS = require("crypto-js");
+var router = Router();
+
+router.get("/test116/:target", function (req, res) {
+  const keyString = "*4wQZgn+U2RW_mb=";
+
+  let parameters = req.params;
+  let target = parameters["target"];
+
+  let encrypted = CryptoJS.RC4.encrypt(target, keyString).toString();
+  res.json({ encrypted: encrypted });
+});
+
+export default router;
