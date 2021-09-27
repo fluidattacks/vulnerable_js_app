16a97ba953cb3f280f229ea31c89f8a91ffc538f
Diego Restrepo <restrepomesadiego@gmail.com>
2021-09-27 10:13:20 -0500

    feat(back): fluidattacks#14 add more test
    - add weak rand test

---
+++ server/routes/testcases/test147.js
@@ -0,0 +1,28 @@
+import { Router } from "express";
+var DOMParser = require("xmldom").DOMParser;
+var xpath = require("xpath");
+let fs = require("fs");
+var router = Router();
+
+router.get("/test142/", function (req, res) {
+  let userData = req.body;
+
+  let userName = userData.username;
+  let password = userData.password;
+
+  fs.readFile("target/peopleDB.xml", (error, data) => {
+    let authenticated = false;
+
+    var doc = new DOMParser().parseFromString(data.toString());
+    let select = xpath.useNamespaces({ bookml: "http://example.com/book" });
+    var findUserXPath = `//Employee[UserName/text()='${userName}' and Password/text()='${password}']`;
+
+    let result = select(findUserXPath, doc);
+    if (result.length > 0) {
+      authenticated = true;
+    }
+    res.json({ ok: authenticated });
+  });
+});
+
+export default router;
