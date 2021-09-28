7f115f458ad29b5424b2e1272938cb3c3aa60391
Diego Restrepo <restrepomesadiego@gmail.com>
2021-09-27 19:07:57 -0500

    feat(back): fluidattacks#14 add more test
    - add weak rand test

---
+++ server/routes/testcases/test153.js
@@ -0,0 +1,21 @@
+import { Router } from "express";
+var router = Router();
+
+router.get("/test153", function (req, res) {
+  let key = "";
+  var data = ["safe"];
+  data.push("safe");
+  data.push(Math.random().toString());
+  data[3] = "safe";
+  data.pop();
+  data.pop();
+
+  for (var currentKey of data) {
+    key = key.concat(currentKey);
+  }
+
+  res.cookie("rememberKey", key);
+  res.json({ ok: true });
+});
+
+export default router;
