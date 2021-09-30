9cbd5c056b365a84719f279e11cde75765c5331f
Diego Restrepo <restrepomesadiego@gmail.com>
2021-09-30 11:58:58 -0500

    feat(back): fluidattacks#14 add environment
    - add database connection information

---
+++ server/helpers/environment.js
@@ -0,0 +1,11 @@
+const Environment = {
+  db: {
+    username: "root",
+    password: "oyster",
+    port: 3306,
+    database: "vulnerable_js_app",
+    host: "localhost",
+  },
+};
+
+module.exports = Environment;
